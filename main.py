0fcfdd15aea1551f6bda6ca104ee728bf51d931d
mhmod313 <140379186+mhmod313@users.noreply.github.com>
2024-06-22 10:50:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,295 +1,38 @@
-
-#import system libs
 import os
-import time
-import shutil
-import pathlib
-import itertools
 
-# import data handling tools
+# Limit the number of threads used by various libraries
+# Now import TensorFlow and other modules
+import tensorflow as tf
 import cv2
 import numpy as np
-import pandas as pd
-import seaborn as sns
-sns.set_style('darkgrid')
-import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix, classification_report
-
-# import Deep learning Libraries
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.optimizers import Adam, Adamax
-from tensorflow.keras.metrics import categorical_crossentropy
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Activation, Dropout, BatchNormalization
-from tensorflow.keras import regularizers
-
-# Ignore Warnings
-import warnings
-warnings.filterwarnings("ignore")
-
-print ('modules loaded')
-
 from flask import Flask, request, jsonify
+#import logging
+from flask_ngrok import run_with_ngrok
+# Initialize Flask app and CORS
 app = Flask(__name__)
-
-# Generate data paths with labels
-data_dir = r'C:\\Users\\USER\\Desktop\\final projects\\train'
-filepaths = []
-labels = []
-
-folds = os.listdir(data_dir)
-for fold in folds:
-    foldpath = os.path.join(data_dir, fold)
-    filelist = os.listdir(foldpath)
-    for file in filelist:
-        fpath = os.path.join(foldpath, file)
-        filepaths.append(fpath)
-        labels.append(fold)
-
-# Concatenate data paths with labels into one dataframe
-Fseries = pd.Series(filepaths, name= 'filepaths')
-Lseries = pd.Series(labels, name='labels')
-df = pd.concat([Fseries, Lseries], axis= 1)
-
-df
-
-# train dataframe
-train_df, valid_df = train_test_split(df,  train_size= 0.8, random_state= 123)
-
-# Generate data paths with labels
-data_dir =  r'C:\\Users\\USER\\Desktop\\final projects\\test'
-filepaths = []
-labels = []
-
-folds = os.listdir(data_dir)
-for fold in folds:
-    foldpath = os.path.join(data_dir, fold)
-    filelist = os.listdir(foldpath)
-    for file in filelist:
-        fpath = os.path.join(foldpath, file)
-        filepaths.append(fpath)
-        labels.append(fold)
-
-# Concatenate data paths with labels into one dataframe
-Fseries = pd.Series(filepaths, name= 'filepaths')
-Lseries = pd.Series(labels, name='labels')
-test_df = pd.concat([Fseries, Lseries], axis= 1)
-
-# crobed image size
-batch_size = 16
-img_size = (224, 224)
-channels = 3
-img_shape = (img_size[0], img_size[1], channels)
-
-# Recommended : use custom function for test data batch size, else we can use normal batch size.
-ts_length = len(test_df)
-test_batch_size = max(sorted([ts_length // n for n in range(1, ts_length + 1) if ts_length%n == 0 and ts_length/n <= 80]))
-test_steps = ts_length // test_batch_size
-
-# This function which will be used in image data generator for data augmentation, it just take the image and return it again.
-def scalar(img):
-    return img
-
-tr_gen = ImageDataGenerator(preprocessing_function= scalar)
-ts_gen = ImageDataGenerator(preprocessing_function= scalar)
-
-train_gen = tr_gen.flow_from_dataframe( train_df, x_col= 'filepaths', y_col= 'labels', target_size= img_size, class_mode= 'categorical',
-                                    color_mode= 'rgb', shuffle= True, batch_size= batch_size)
-
-valid_gen = ts_gen.flow_from_dataframe( valid_df, x_col= 'filepaths', y_col= 'labels', target_size= img_size, class_mode= 'categorical',
-                                    color_mode= 'rgb', shuffle= True, batch_size= batch_size)
-
-# Note: we will use custom test_batch_size, and make shuffle= false
-test_gen = ts_gen.flow_from_dataframe( test_df, x_col= 'filepaths', y_col= 'labels', target_size= img_size, class_mode= 'categorical',
-                                    color_mode= 'rgb', shuffle= False, batch_size= test_batch_size)
-
-
-g_dict = train_gen.class_indices      # defines dictionary {'class': index}
-classes = list(g_dict.keys())       # defines list of dictionary's kays (classes), classes names : string
-images, labels = next(train_gen)      # get a batch size samples from the generator
-
-plt.figure(figsize= (20, 20))
-
-for i in range(16):
-    plt.subplot(4, 4, i + 1)
-    image = images[i] / 255       # scales data to range (0 - 255)
-    plt.imshow(image)
-    index = np.argmax(labels[i])  # get image index
-    class_name = classes[index]   # get class of image
-    plt.title(class_name, color= 'blue', fontsize= 12)
-    plt.axis('off')
-plt.show()
-
-# Create Model Structure
-img_size = (224, 224)
-channels = 3
-img_shape = (img_size[0], img_size[1], channels)
-class_count = len(list(train_gen.class_indices.keys())) # to define number of classes in dense layer
-
-# create pre-trained model (you can built on pretrained model such as :  efficientnet, VGG , Resnet )
-# we will use efficientnetb3 from EfficientNet family.
-base_model = tf.keras.applications.efficientnet.EfficientNetB0(include_top= False, weights= "imagenet", input_shape= img_shape, pooling= 'max')
-base_model.trainable = False
-
-model = Sequential([
-    base_model,
-    BatchNormalization(axis= -1, momentum= 0.99, epsilon= 0.001),
-    Dense(256, kernel_regularizer= regularizers.l2(l= 0.016), activity_regularizer= regularizers.l1(0.006),
-                bias_regularizer= regularizers.l1(0.006), activation= 'relu'),
-    Dropout(rate= 0.45, seed= 123),
-    Dense(class_count, activation= 'softmax')
-])
-
-model.compile(Adamax(learning_rate= 0.001), loss= 'categorical_crossentropy', metrics= ['accuracy'])
-#model.compile(optimizer=Adam(learning_rate=0.0001), loss='binary_crossentropy', metrics=['accuracy'])
-
-model.summary()
-
-batch_size = 32   # set batch size for training
-epochs = 10   # number of all epochs in training
-
-history = model.fit(x= train_gen, epochs= epochs, verbose= 1, validation_data= valid_gen, 
-                    validation_steps= None, shuffle= False)
-
-# Define needed variables
-tr_acc = history.history['accuracy']
-tr_loss = history.history['loss']
-val_acc = history.history['val_accuracy']
-val_loss = history.history['val_loss']
-index_loss = np.argmin(val_loss)
-val_lowest = val_loss[index_loss]
-index_acc = np.argmax(val_acc)
-acc_highest = val_acc[index_acc]
-Epochs = [i+1 for i in range(len(tr_acc))]
-loss_label = f'best epoch= {str(index_loss + 1)}'
-acc_label = f'best epoch= {str(index_acc + 1)}'
-
-# Plot training history
-plt.figure(figsize= (20, 8))
-plt.style.use('fivethirtyeight')
-
-plt.subplot(1, 2, 1)
-plt.plot(Epochs, tr_loss, 'r', label= 'Training loss')
-plt.plot(Epochs, val_loss, 'g', label= 'Validation loss')
-plt.scatter(index_loss + 1, val_lowest, s= 150, c= 'blue', label= loss_label)
-plt.title('Training and Validation Loss')
-plt.xlabel('Epochs')
-plt.ylabel('Loss')
-plt.legend()
-
-plt.subplot(1, 2, 2)
-plt.plot(Epochs, tr_acc, 'r', label= 'Training Accuracy')
-plt.plot(Epochs, val_acc, 'g', label= 'Validation Accuracy')
-plt.scatter(index_acc + 1 , acc_highest, s= 150, c= 'blue', label= acc_label)
-plt.title('Training and Validation Accuracy')
-plt.xlabel('Epochs')
-plt.ylabel('Accuracy')
-plt.legend()
-
-plt.tight_layout
-plt.show()
-
-ts_length = len(test_df)
-test_batch_size = max(sorted([ts_length // n for n in range(1, ts_length + 1) if ts_length%n == 0 and ts_length/n <= 80]))
-test_steps = ts_length // test_batch_size
-
-train_score = model.evaluate(train_gen, steps= test_steps, verbose= 1)
-valid_score = model.evaluate(valid_gen, steps= test_steps, verbose= 1)
-test_score = model.evaluate(test_gen, steps= test_steps, verbose= 1)
-
-print("Train Loss: ", train_score[0])
-print("Train Accuracy: ", train_score[1])
-print('-' * 20)
-print("Validation Loss: ", valid_score[0])
-print("Validation Accuracy: ", valid_score[1])
-print('-' * 20)
-print("Test Loss: ", test_score[0])
-print("Test Accuracy: ", test_score[1])
-
-preds = model.predict_generator(test_gen)
-y_pred = np.argmax(preds, axis=1)
-
-print(y_pred)
-
-g_dict = test_gen.class_indices
-classes = list(g_dict.keys())
-
-# Confusion matrix
-cm = confusion_matrix(test_gen.classes, y_pred)
-
-plt.figure(figsize= (10, 10))
-plt.imshow(cm, interpolation= 'nearest', cmap= plt.cm.Blues)
-plt.title('Confusion Matrix')
-plt.colorbar()
-
-tick_marks = np.arange(len(classes))
-plt.xticks(tick_marks, classes, rotation= 45)
-plt.yticks(tick_marks, classes)
-
-
-thresh = cm.max() / 2.
-for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-    plt.text(j, i, cm[i, j], horizontalalignment= 'center', color= 'white' if cm[i, j] > thresh else 'black')
-
-plt.tight_layout()
-plt.ylabel('True Label')
-plt.xlabel('Predicted Label')
-
-plt.show()
-
-# Classification report
-print(classification_report(test_gen.classes, y_pred, target_names= classes))
-
-model_name = model.input_names[0][:-6]
-subject = 'EyeDetection'
-acc = test_score[1] * 100
-save_path = 'C:\\Users\\USER\\Desktop\\x'
-
-# Save model
-save_id = str(f'{model_name}-{subject}-{"%.2f" %round(acc, 2)}.h5')
-model_save_loc = os.path.join(save_path, save_id)
-model.save(model_save_loc)
-print(f'model was saved as {model_save_loc}')
-
-# Save weights
-weight_save_id = str(f'{model_name}-{subject}-weights.h5')
-weights_save_loc = os.path.join(save_path, weight_save_id)
-model.save_weights(weights_save_loc)
-print(f'weights were saved as {weights_save_loc}')
-
-
-from tensorflow.keras.models import load_model
-
 # Load your model
-model = tf.keras.models.load_model('C:\\Users\\USER\\Desktop\\x\\efficientnetb0-EyeDetection-93.96.h5')
-
-classes = ['open eye', 'close eye']
+model = tf.keras.models.load_model('C:\\Users\\USER\\Desktop\\x\\g.h5')
+if model!=None:
+    print("Model loaded successfully!")
+# Define classes and image size
+classes = ['closeeye', 'openeye']
 img_size = (224, 224)
-
 # Define a function to preprocess the image
 def preprocess_image(image):
     image = cv2.resize(image, img_size)
-    image = image / 255.0  # Normalize pixel values to [0, 1]
+    image = image.astype('float32')
     image = np.expand_dims(image, axis=0)  # Add batch dimension
     return image
-
-# Define endpoint to predict image
-@app.route('/classify', methods=['POST'])
-def classify_image():
-    if 'image' not in request.files:
-        return jsonify({'error': 'No image provided'})
-
+@app.route('/', methods=['POST'])
+def classify_eye_state():
     file = request.files['image']
-    image = cv2.imdecode(np.fromstring(file.read(), np.uint8), cv2.IMREAD_COLOR)
+    file_bytes = np.frombuffer(file.read(), np.uint8)
+    #image = cv2.imread(file) 
+    image = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
     preprocessed_image = preprocess_image(image)
     predictions = model.predict(preprocessed_image)
     predicted_class_index = np.argmax(predictions)
     predicted_class = classes[predicted_class_index]
-
     return jsonify({'class': predicted_class})
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0')
